f937780aa5dbea85a54771b67bd90981e59e4825
Paulina Kaczmarek <kaczmarekpaulina@yahoo.com>
2025-11-22 13:18:33 +1100

    start of project frontend and backend

---
+++ client/src/components/card.tsx
@@ -0,0 +1,10 @@
+export default function Card() {
+  return (
+    <div>
+      <img></img>
+      <h1></h1>
+      <p></p>
+      <a></a>
+    </div>
+  );
+}
---
+++ client/src/components/search.tsx
@@ -0,0 +1,11 @@
+export default function Search() {
+  return (
+    <div>
+      <form>
+        <label htmlFor="addInput"></label>
+        <input id="addInput" type="add"></input>
+        <button type="submit">ADD</button>
+      </form>
+    </div>
+  );
+}
