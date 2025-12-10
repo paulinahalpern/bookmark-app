8ad89229abc146efc58dd9eadb581c43f0167f74
Paulina Kaczmarek <kaczmarekpaulina@yahoo.com>
2025-12-10 20:23:42 +1100

    requested changes

--- bookmark-app-api/routes/users.js
+++ bookmark-app-api/routes/users.js
@@ -3,7 +3,6 @@ let router = express.Router();
 const { isLoggedIn } = require("../lib/ensure-login");
 
 router.get("/users/me", isLoggedIn, async (req, res) => {
-  console.log(req.user);
   res.json(req.user);
 });
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,10 @@ import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter } from "react-router-dom";
 
-
 createRoot(document.getElementById("root")!).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-    <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </GoogleOAuthProvider>
 );
